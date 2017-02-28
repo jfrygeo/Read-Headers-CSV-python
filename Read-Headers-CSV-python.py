1e5ae1e7fb21a19f0eb40fd00ca2afed3dd0260b
John Fry <jfry@esri.com>
2017-02-28 11:24:24 -0500

    Edited script to get lines and header information
    Created two functions that count the lines and display the headers of
    the file

--- Read-Headers-CSV-python.py
+++ Read-Headers-CSV-python.py
@@ -1,13 +1,27 @@
 #Reads one line in a large csv file
+#Gives total count of lines csv
 # __author__ = 'john6807'
-import csv, os
+import csv,os
 
 #Input the filepath to your csv file: i.e. "C:\\MyFolder\\MyCSV.csv"
-filepath = r"C:\\MyFolder\\MyCSV.csv"
+#filename = "C:\\MyFolder\\MyCSV.csv"
+filename = os.path.join(os.path.dirname(__file__)+"\SampleData\TestCSV.csv")
 
-with open(filepath, 'r') as infile:
+#Depending on your data source, you may need to change the encoding(i.e. ('utf-8','utf-16')
+infile = open(filename, 'r', encoding=("iso-8859-15"))
+
+# Read the headers of the csv file
+def file_headers(infile):
     reader = csv.DictReader(infile)
     fieldnames = reader.fieldnames
     print (fieldnames)
+file_headers(infile)
 
-os.system("pause")
+#Read the length of the file
+def file_length(infile):
+    for i, length in enumerate(infile):
+        pass
+    print (str(i + 2) + " Total lines and headers")
+file_length(infile)
+
+exit()
